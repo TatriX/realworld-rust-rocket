330ef2fd1b98f47e9895a38666b217219bd88383
TatriX <tatrics@gmail.com>
2018-01-18 23:25:34 +0300

    Fix errors responder

--- src/errors.rs
+++ src/errors.rs
@@ -1,22 +1,39 @@
-use rocket::http::Status;
+use rocket::response::status;
 use rocket::request::Request;
-use rocket::response::{self, Responder, Response};
+use rocket::http::Status;
+use rocket::response::{self, Responder};
 use rocket_contrib::Json;
 use validator::ValidationErrors;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
+use std::collections::HashMap;
+use std::error::Error;
 
-#[derive(Serialize)]
+#[derive(Debug, Serialize)]
 pub struct Errors {
     pub errors: ValidationErrors,
 }
 
 impl<'r> Responder<'r> for Errors {
     fn respond_to(self, req: &Request) -> response::Result<'r> {
-        Json(self).respond_to(req)
+        // TODO: get rid of allocations
+        let mut errors = HashMap::new();
+        for (field, ers) in self.errors.inner() {
+            errors.insert(
+                field,
+                ers.into_iter()
+                    .map(|err| err.description().to_owned())
+                    .collect::<Vec<_>>(),
+            );
+        }
+        status::Custom(
+            Status::UnprocessableEntity,
+            Json(json!({ "errors": errors })),
+        ).respond_to(req)
     }
 }
 
 impl Errors {
+    #[allow(dead_code)]
     fn new() -> Errors {
         Errors {
             errors: ValidationErrors::new(),
@@ -29,5 +46,11 @@ impl Deref for Errors {
 
     fn deref(&self) -> &ValidationErrors {
         &self.errors
+    }
+}
+
+impl DerefMut for Errors {
+    fn deref_mut(&mut self) -> &mut ValidationErrors {
+        &mut self.errors
     }
 }
