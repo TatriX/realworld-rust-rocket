3eb982d44e0c2a2b603045a17d5ec51acd277bb5
TatriX <tatrics@gmail.com>
2018-03-04 20:31:47 +0300

    Update auth tests

--- tests/auth.rs
+++ tests/auth.rs
@@ -5,14 +5,26 @@ extern crate serde_derive;
 #[macro_use]
 extern crate serde_json;
 
+#[macro_use]
+extern crate lazy_static;
+
+extern crate hyper;
+
+use reqwest::Response;
 use serde_json::Value;
 use std::collections::HashMap;
+use hyper::header::{Authorization, Headers};
+use std::sync::RwLock;
 
 const API_URL: &'static str = "http://localhost:8000/api";
 
 const USERNAME: &'static str = "rust-diesel-rocket";
 const EMAIL: &'static str = "rust-diesel-rocket@example.com";
 const PASSWORD: &'static str = "qweasdzxc";
+
+lazy_static! {
+    static ref TOKEN: RwLock<String> = RwLock::new("".to_string());
+}
 
 #[derive(Debug, Deserialize)]
 struct ValidationErrors {
@@ -23,7 +35,7 @@ fn make_url(path: &str) -> String {
     format!("{}/{}", API_URL, path)
 }
 
-fn post(path: &str, json: Value) -> reqwest::Response {
+fn post(path: &str, json: Value) -> Response {
     reqwest::Client::new()
         .post(&make_url(path))
         .json(&json)
@@ -31,7 +43,40 @@ fn post(path: &str, json: Value) -> reqwest::Response {
         .expect(&format!("{} {:#?}: post error", path, json))
 }
 
-#[test]
+fn get(path: &str) -> Response {
+    let token = TOKEN.read().unwrap().to_string();
+    let mut headers = Headers::new();
+    headers.set(Authorization(format!("Token {}", token)));
+
+    reqwest::Client::new()
+        .get(&make_url(path))
+        .headers(headers)
+        .send()
+        .expect(&format!("{} get error", path))
+}
+
+fn check_response<F>(resp: &mut Response, f: F)
+where
+    F: FnOnce(&mut Response),
+{
+    let status = resp.status();
+    match status {
+        reqwest::StatusCode::Ok => f(resp),
+        _ => panic!("Got status: {}", status),
+    }
+}
+
+fn check_user_response(resp: &mut Response) {
+    let wrapper = resp.json::<Value>().expect("Can't parse user");
+    let user = wrapper.get("user").expect("Must have a 'user' field");
+
+    assert_eq!(user.get("email").expect("User has email"), EMAIL);
+    assert_eq!(user.get("username").expect("User has username"), USERNAME);
+    assert!(user.get("bio").is_some());
+    assert!(user.get("image").is_some());
+    assert!(user.get("token").is_some());
+}
+
 fn register() {
     let mut resp = post(
         "users",
@@ -39,16 +84,7 @@ fn register() {
     );
     let status = resp.status();
     match status {
-        reqwest::StatusCode::Ok => {
-            let wrapper = resp.json::<Value>().expect("Can't parse user");
-            let user = wrapper.get("user").expect("Must have a 'user' field");
-
-            assert_eq!(user.get("email").expect("User has email"), EMAIL);
-            assert_eq!(user.get("username").expect("User has username"), USERNAME);
-            assert!(user.get("bio").is_some());
-            assert!(user.get("image").is_some());
-            assert!(user.get("token").is_some());
-        }
+        reqwest::StatusCode::Ok => check_user_response(&mut resp),
         reqwest::StatusCode::UnprocessableEntity => {
             let body = resp.json::<ValidationErrors>()
                 .expect("Can't parse validation errors");
@@ -58,4 +94,48 @@ fn register() {
         }
         _ => panic!("Got status: {}", status),
     }
+}
+
+fn login() {
+    check_response(
+        &mut post(
+            "users/login",
+            json!({"user": {"email": EMAIL, "password": PASSWORD}}),
+        ),
+        check_user_response,
+    );
+}
+
+fn login_and_save_credentials() {
+    check_response(
+        &mut post(
+            "users/login",
+            json!({"user": {"email": EMAIL, "password": PASSWORD}}),
+        ),
+        |resp| {
+            let wrapper = resp.json::<Value>().expect("Can't parse user");
+            let user = wrapper.get("user").expect("Must have a 'user' field");
+            let mut token = TOKEN.write().unwrap();
+            *token = user.get("token")
+                .expect("User has token")
+                .as_str()
+                .expect("Token must be a string")
+                .to_string();
+        },
+    );
+}
+
+fn current_user() {
+    check_response(&mut get("user"), check_user_response)
+}
+
+// We need to run the tests in order. And default test framework do
+// that in random order. So for now let's just run them from the
+// "main" test function.
+#[test]
+fn auth() {
+    register();
+    login();
+    login_and_save_credentials();
+    current_user();
 }
