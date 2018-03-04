3c0d71f94eb83c7f31e0d7e8176fafd976203ab0
TatriX <tatrics@gmail.com>
2018-03-04 19:09:51 +0300

    Trying reqwest based tests

--- tests/auth.rs
+++ tests/auth.rs
@@ -2,6 +2,11 @@ extern crate reqwest;
 
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+
+use serde_json::Value;
+use std::collections::HashMap;
 
 const API_URL: &'static str = "http://localhost:8000/api";
 
@@ -9,29 +14,40 @@ const USERNAME: &'static str = "rust-diesel-rocket";
 const EMAIL: &'static str = "rust-diesel-rocket@example.com";
 const PASSWORD: &'static str = "qweasdzxc";
 
+#[derive(Debug, Deserialize)]
+struct ValidationErrors {
+    errors: HashMap<String, Vec<String>>,
+}
+
 fn make_url(path: &str) -> String {
     format!("{}/{}", API_URL, path)
 }
 
-#[derive(Serialize)]
-struct SignUp<'a> {
-    username: &'a str,
-    email: &'a str,
-    password: &'a str,
+fn post(path: &str, json: Value) -> reqwest::Response {
+    reqwest::Client::new()
+        .post(&make_url(path))
+        .json(&json)
+        .send()
+        .expect(&format!("{} {:#?}: post error", path, json))
 }
-
 
 #[test]
 fn register() {
-    let response = reqwest::Client::new()
-        .post(&make_url("users"))
-        .json(&SignUp{
-            username: USERNAME,
-            email: EMAIL,
-            password: PASSWORD,
-        })
-        .send()
-        .expect("register should work");
-    println!("{:?}", response);
-    panic!();
+    let mut resp: reqwest::Response = post(
+        "users",
+        json! ({"user": {"username": USERNAME, "email": EMAIL, "password": PASSWORD}}),
+    );
+    let status = resp.status();
+    match status {
+        reqwest::StatusCode::Ok => {
+            // everything is fine
+        }
+        reqwest::StatusCode::UnprocessableEntity => {
+            let body = resp.json::<ValidationErrors>().unwrap();
+            if body.errors["username"] != vec!["has already been taken"] {
+                panic!("Got validation errors: {:#?}", body);
+            }
+        }
+        _ => panic!("Got status: {}", status),
+    }
 }
