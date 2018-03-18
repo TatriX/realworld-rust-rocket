82e7d39f9265291e756ccd8fcb2679631d68710d
Anish Karandikar <anishkny@gmail.com>
2018-03-18 12:40:31 -0400

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
-CREATE USER realworld IDENTIFIED BY 'realworld';
+
 CREATE DATABASE realworld;
+USE realworld;
+CREATE USER realworld IDENTIFIED BY 'realworld';
 GRANT ALL ON realworld TO realworld;
