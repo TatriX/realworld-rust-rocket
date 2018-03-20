fa46667de9e5504a9c02afcc2f7890f8ee3165f2
Anish Karandikar <anishkny@gmail.com>
2018-03-20 10:47:35 -0400

    test: Enable newman API tests (#3)
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * chore(ci): Run API tests with newman (#2)

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-newman run Conduit.postman_collection.json -e Conduit.postman_integration_test_environment.json --global-var "EMAIL=tester@test.test" --global-var "PASSWORD=qwerty" "$@"
+newman run ./Conduit.postman_collection.json --global-var "apiUrl=http://localhost:8000/api"
