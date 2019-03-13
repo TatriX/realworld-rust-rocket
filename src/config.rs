20207bda7d0c2f457de4e67e2b38c26a9322b471
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-03-12 21:01:05 -0500

    style: Removed non necessary static life time in global constants

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 /// js toISOString() in test suit can't handle chrono's default precision
-pub const DATE_FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S%.3fZ";
+pub const DATE_FORMAT: &str = "%Y-%m-%dT%H:%M:%S%.3fZ";
 
-pub const SECRET: &'static str = "secret123";
-pub const TOKEN_PREFIX: &'static str = "Token ";
+pub const SECRET: &str = "secret123";
+pub const TOKEN_PREFIX: &str = "Token ";
