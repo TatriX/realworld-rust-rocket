7a7d3ccb97babe053c291d6b8af2e078470c4b39
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-03-12 19:04:56 -0500

    refact: Beautify types in db

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -15,6 +15,7 @@ use rocket::request::{self, FromRequest};
 use rocket::{Outcome, Request, State};
 
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+pub type PooledConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
 
 pub fn init_pool() -> Pool {
     dotenv().ok();
@@ -24,7 +25,7 @@ pub fn init_pool() -> Pool {
     r2d2::Pool::new(manager).expect("db pool")
 }
 
-pub struct Conn(pub r2d2::PooledConnection<ConnectionManager<PgConnection>>);
+pub struct Conn(pub PooledConnection);
 
 impl Deref for Conn {
     type Target = PgConnection;
@@ -38,7 +39,7 @@ impl Deref for Conn {
 impl<'a, 'r> FromRequest<'a, 'r> for Conn {
     type Error = ();
 
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Conn, ()> {
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Conn, Self::Error> {
         let pool = request.guard::<State<Pool>>()?;
         match pool.get() {
             Ok(conn) => Outcome::Success(Conn(conn)),
