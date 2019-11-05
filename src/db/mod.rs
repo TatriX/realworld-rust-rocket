ee766d894e97da4502cad81b707a2e2b871416e9
SRugina <dev@srugina.com>
2019-11-05 19:09:26 +0000

    Fixes #16 returning incorrect articles count
    - bug fix where articlesCount returned was not of total articles in db
    
    - trait OffsetLimit (acts like pagination) created

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -36,3 +36,64 @@ pub fn config() -> Config {
 
 #[database("diesel_postgres_pool")]
 pub struct Conn(diesel::PgConnection);
+
+use diesel::prelude::*;
+use diesel::query_dsl::methods::LoadQuery;
+use diesel::query_builder::*;
+use diesel::pg::Pg;
+use diesel::sql_types::BigInt;
+
+pub trait OffsetLimit: Sized {
+    fn offset_and_limit(self, offset: i64, limit: i64) -> OffsetLimited<Self>;
+}
+
+impl<T> OffsetLimit for T {
+    fn offset_and_limit(self, offset: i64, limit: i64) -> OffsetLimited<Self> {
+        OffsetLimited {
+            query: self,
+            limit,
+            offset,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, QueryId)]
+pub struct OffsetLimited<T> {
+    query: T,
+    offset: i64,
+    limit: i64,
+}
+
+impl<T> OffsetLimited<T> {
+
+    pub fn load_and_count<U>(self, conn: &PgConnection) -> QueryResult<(Vec<U>, i64)>
+    where
+        Self: LoadQuery<PgConnection, (U, i64)>,
+    {
+        let results = self.load::<(U, i64)>(conn)?;
+        let total = results.get(0).map(|x| x.1).unwrap_or(0);
+        let records = results.into_iter().map(|x| x.0).collect();
+        Ok((records, total))
+    }
+}
+
+impl<T: Query> Query for OffsetLimited<T> {
+    type SqlType = (T::SqlType, BigInt);
+}
+
+impl<T> RunQueryDsl<PgConnection> for OffsetLimited<T> {}
+
+impl<T> QueryFragment<Pg> for OffsetLimited<T>
+where
+    T: QueryFragment<Pg>,
+{
+    fn walk_ast(&self, mut out: AstPass<Pg>) -> QueryResult<()> {
+        out.push_sql("SELECT *, COUNT(*) OVER () FROM (");
+        self.query.walk_ast(out.reborrow())?;
+        out.push_sql(") t LIMIT ");
+        out.push_bind_param::<BigInt, _>(&self.limit)?;
+        out.push_sql(" OFFSET ");
+        out.push_bind_param::<BigInt, _>(&self.offset)?;
+        Ok(())
+    }
+}
