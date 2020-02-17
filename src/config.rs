286e96fef22d4de7e07e6db9c3994c9e39c37967
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-17 14:27:17 +0000

    fix: Removing debug assertion for release build

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,6 @@ use std::collections::HashMap;
 use std::env;
 
 /// Debug only secret for JWT encoding & decoding.
-#[cfg(debug_assertions)]
 const SECRET: &'static str = "8Xui8SN4mI+7egV/9dlfYYLGQJeEx4+DwmSQLwDVXJg=";
 
 /// js toISOString() in test suit can't handle chrono's default precision
